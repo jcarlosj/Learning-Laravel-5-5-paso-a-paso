245a6c006b916811533b67920f5bc56ad122328b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-12-28 12:49:10 -0500

    +PHPUnit: Define Prueba Bienvenida a usuarios con o sin nickname

--- tests/Feature/WelcomeUsersTest.php
+++ tests/Feature/WelcomeUsersTest.php
@@ -8,13 +8,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WelcomeUsersTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    /** @test */
+    public function it_welcome_users_with_nickname()
     {
-        $this->assertTrue(true);
+        $this -> get( '/usuarios/juan/jcarlosj' )                            # Simula petición a la URL /usuarios/juan/jcarlosj
+              -> assertStatus( 200 )                                         # Comprueba el estado de la petición
+              -> assertSee( 'Bienvenido juan, tú usuario es: jcarlosj' );    # Comprueba que el código fuente de la página generada se puede ser ese texto
+    }
+    /** @test */
+    public function it_welcome_users_without_nickname()
+    {
+        $this -> get( '/usuarios/juan' )            # Simula petición a la URL /usuarios/juan
+              -> assertStatus( 200 )                # Comprueba el estado de la petición
+              -> assertSee( 'Bienvenido juan' );    # Comprueba que el código fuente de la página generada se puede ser ese texto
     }
 }
