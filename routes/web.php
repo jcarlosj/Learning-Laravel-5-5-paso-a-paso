aab54688823dea9eae14d8b8b83ab8508733e3c1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-12-28 17:28:39 -0500

    +? (-Error): Corrigue error inducido

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route :: get( '/usuario/{nombre}', 'WelcomeController@thenName' )
 
 Route :: get( '/usuarios', 'UserController@index' );
 
-Route :: get( '/usuarios/nuevo', 'UserController@crea' );    # Induce al ERROR al llamar un método que no existe
+Route :: get( '/usuarios/nuevo', 'UserController@create' );    # Corrige ERROR
 
 // Pasando parámetros dinámicos opcionales a una ruta
 Route :: get( '/usuarios/{name}/{nickname?}', 'WelcomeUserController' );# Solo llamamos al controlador sin indicarle el nombre del método
