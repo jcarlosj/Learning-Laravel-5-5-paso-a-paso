9efaf2e5151af6d2c403288cbee816e2ad0c48f9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-12-30 13:43:33 -0500

    +Views (Bootstrap|Blade): Integra Template de Boostrap al proyecto en Laravel
    Aclaración:
    En este caso se ha dividido la plantilla en 2 partes:
    
      - La primera donde se encuentra el encabezado de la plantilla en un
        archivo separado con soporte con Blade, llamado 'header.blade.php'
      - La segunta donde se encuentra el pie de página de la plantilla en
        un archivo separado con soporte con Blade, llamado 'footer.blade.php'
    
    Ahora solo basta con incluir cada una de estas partes dentro de las cada
    una de las vistas que generemos usando la directiba @include de Blade
    a la que le pasamos el nombre del archivo que usa como referencia el
    directorio de /views y qque no requiere las extensiones de los mismos.

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,19 +1,12 @@
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=0, maximum-scale=1.0, min-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Listado de usuarios</title>
-    </head>
-    <body>
-        <h2>{{ $title }}</h2>
-        <ul>
-            @forelse ( $users as $key => $user )
-                <li>{{ $user }}</li>
-            @empty
-                <li>No hay usuarios registrados</li>
-            @endforelse
-        </ul>
-    </body>
-</html>
+@include( 'header' )
+
+<h2>{{ $title }}</h2>
+<ul>
+    @forelse ( $users as $key => $user )
+        <li>{{ $user }}</li>
+    @empty
+        <li>No hay usuarios registrados</li>
+    @endforelse
+</ul>
+
+@include( 'footer' )
