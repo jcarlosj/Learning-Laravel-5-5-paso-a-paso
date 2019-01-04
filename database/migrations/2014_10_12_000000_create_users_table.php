2f158c7d918cda7a15f1cb5c79f307b38440371e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-04 15:34:26 -0500

    -Migration: Modifica tabla 'users', elimina campo/columna 'profession'
    Aclaración:
    Revertimos la acción anterior para ejemplificar como agregar el mismo
    campo de forma no destructiva.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table -> string( 'profession', 50 ) -> nullable();    # Cambia columna/campopara soportar 50 caracteres
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
