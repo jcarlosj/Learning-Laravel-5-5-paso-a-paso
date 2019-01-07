e858abe01094de73100fed28ec7b04f6d8c96878
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-07 10:56:44 -0500

    $Seeder: Refactorización recomendada cuando se desean truncar varias tablas
    Aclaración:
     - Las acciones referentes a una tabla específica deben ir exclusivamente
       en el seeder para el cual son creadas las modificaciones.
     - Si una acción representan acciones en una o más tablas es recomendable
       llevar estas acciones a la clase Padre del cual extienden nuestros Seeders
       y alojar estas acciones allí.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /* Ejecuta Vaciado o Truncado de tablas */
+        $this -> truncateAllTables();
         // $this->call(UsersTableSeeder::class);
         #dd( ProfessionSeeder :: class );               /* Retorna el nombre del Seeder, en este caso: 'ProfessionSeeder' */
 
         /* Registro de Seeders */
         $this -> call( ProfessionSeeder :: class );
+    }
+
+    private function truncateAllTables() {
+        DB :: statement( 'SET FOREIGN_KEY_CHECKS = 0;' );  /* Sentencia SQL para DESHABILITAR revisión de llaves foráneas */
+        DB :: table( 'users' ) -> truncate();              /* Trunca o vacia la tabla 'professions' antes de hacer inserciones */
+        DB :: table( 'professions' ) -> truncate();        /* Trunca o vacia la tabla 'professions' antes de hacer inserciones */
+        DB :: statement( 'SET FOREIGN_KEY_CHECKS = 1;' );  /* Sentencia SQL para HABILITAR revisión de llaves foráneas */
     }
 }
--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,17 +12,6 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
-        DB :: statement( 'SET FOREIGN_KEY_CHECKS = 0;' );  /* Sentencia SQL para DESHABILITAR revisión de llaves foráneas */
-        DB :: table( 'professions' ) -> truncate();        /* Trunca o vacia la tabla 'professions' antes de hacer inserciones */
-        DB :: statement( 'SET FOREIGN_KEY_CHECKS = 1;' );  /* Sentencia SQL para HABILITAR revisión de llaves foráneas */
-        # NOTA: Es importante tener en cuenta no tener restricciones de llave foránea,
-        #       si no no se podrá truncar la tabla.
-        # SOLUCION:
-        #  - Comentar la línea de creación de la llave forárea, ejecutar una migración
-        #    para realizar cambios y luego ejecutar el seeder, reestableciendo la llave
-        #    foránea al finalizar el proceso
-        #  - Deshabilitar la revisión de llaves foráneas de nuestro motor de bases de datos
-
         /* Realiza inserciones a nuestra tabla 'professions' */
         DB :: table( 'professions' ) -> insert([
             'title' => 'BackEnd Developer'
