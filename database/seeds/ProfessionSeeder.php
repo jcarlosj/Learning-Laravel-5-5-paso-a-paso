57ba7c2acf532a98c9f6d1172a8c9cd3216d9fb6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-08 11:05:54 -0500

    +Seeder: Inserta usando consultas SQL
    Aclaración:
    Para poder usar consultas SQL se debe usar el constructor de consultas
    de Laravel que se apoya en el Facade DB usando su método insert()
    
    Existen 3 formas de ejecutar SQL "puro" dentro de Laravel
    
       - Forma 1: Se pasa como parámetro la consulta SQL directamente
                  al método insert(), siempre que los valores a insertar
                  no vengan directamente de los usuarios
       - Forma 2: Se pasa como primer parámetro la consulta SQL con una
                  variación en la que se usa el signo de interrogación
                  como un marcador de posición, como segundo parámetro
                  se pasa un Array con el valor o campo a insertar
       - Forma 3: Se pasa como primer parámetro la consulta SQL con variaciones
                  como parámetros de sustitución con nombres específicos
                  en las que se usan los dos puntos como prefijo de dicho nombre
                  que será específico para cada campo que se desea insertar,
                  como segunto parámetro se pasa un Array ASOCIATIVO en el
                  que se pasan los valores o campos a insertar, especificando
                  su nombre.
    
    NOTA: En las formas 2 y 3 se pretente escapar cualquier irregularidad dentro
          de la cadena de sentencia y variables que pueda usar para insertar datos
          Evitando así la inyección de SQL, normalmente estas formas se usan cuando
          Los valores VIENEN del usuario a través de las vistas o formularios.

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,6 +12,17 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+        /* Realiza inserciones usando sentencias SQL a través del método insert() */
+        DB :: insert( 'INSERT INTO professions ( title ) VALUES ( "FullStack JavaScript Developer" ) ' );      # SIN marcadores de posición (cuando los datos NO VIENEN directamente de los usuarios), la cadena se pasa con comillas dobles
+
+        DB :: insert( 'INSERT INTO professions ( title ) VALUES ( ? ) ', [ 'Android Mobile Developer' ] );     # CON marcadores de posición (cuando los datos VIENEN directamente de los usuarios, es decir el usuario envia el dato y este esta alojado en una variable), la cadena se pasa como un Array
+        # NOTA: Debemos usar un marcador ? para indicar la posición de un parámetro dinámico, que nos protegerá de inyección SQL en el valor pasado
+
+        DB :: insert( 'INSERT INTO professions ( title ) VALUES ( :title )', [                                 # CON parámetros de sustitución (cuando los datos VIENEN directamente de los usuarios ), la cadena se pasa como un Array asociativo
+            'title' => 'iOS Mobile Developer'
+        ]);
+        # NOTA: Cuando hay muchos parámetros lo mejor es usar un parámetro de sustitución con nombre, que también nos protegerá de la inyección SQL
+
         /* Realiza inserciones a nuestra tabla 'professions' */
         DB :: table( 'professions' ) -> insert([
             'title' => 'BackEnd Developer'
