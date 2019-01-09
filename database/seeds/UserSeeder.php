4b5c2042854730ac6702ce5da8c243fa22c0f482
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-09 09:32:31 -0500

    +Seeder: Ejemplos usando el Constructor de consultas de Laravel
    Aclaración:
    El resultado obtenido al usar el constructor de consultas de Laravel es
    un objeto de la clase Illuminate\Support\Collection que encapsula un
    Array de datos de los resultados extraidos de dicha consulta.
    
    Entre las grandes ventajas que nos trae usar este método es que podemos
    usar una interfaz orientada a objetos con la cual trabajar y muchas
    funciones extra, en estos ejemplos se usan algunas de ellas:
    
      - select(): Método que permite indicar el nombre del campo que deseamos obtener
      - first(): Método que permite obtener el primer resultado de la consulta
      - take(): Método que nos permite indicar el limite de resultados que deseamos
      - get(): Método que nos permite obtener el valor de una consulta realizada
    
    NOTA: Cada uno de estos métodos requieren o no el paso de parámetros para operar
          correctamente. Hay muchos más metodos que facilitan la tarea de operar
          los datos obtenidos por el constructor de consultas de Laravel

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        /* Ejemplos de consultas SQL usando el método select() del Facade DB de Lavavel */
+        /* Ejemplos de consultas SQL usando el método select() del Facade DB de Lavavel
         $professions_1 = DB :: select( 'SELECT id FROM professions WHERE title = ?', [ 'BackEnd Developer' ] );                # Obtiene el ID del campo donde title es "BackEnd Developer"
         $professions_2 = DB :: select( 'SELECT id FROM professions WHERE title = ? LIMIT 0,1', [ 'BackEnd Developer' ] );      # Obtiene un ID o ninguno si el campo title es "BackEnd Developer"
         $professions_3 = DB :: select( 'SELECT id FROM professions LIMIT 0,1' );                                               # Obtiene el primer ID registrado en la tabla, siempre que existan registros en ella
@@ -20,6 +20,19 @@ class UserSeeder extends Seeder
             'Consulta 1: SELECT id FROM professions WHERE title = ?', $professions_1,
             'Consulta 2: SELECT id FROM professions WHERE title = ? LIMIT 0,1', $professions_2,
             'Consulta 3: SELECT id FROM professions LIMIT 0,1', $professions_3
+        );*/
+
+        /* Ejemplos usando el Constructor de consultas de Laravel */
+        $professions_1 = DB :: table( 'professions' ) -> select( 'id' ) -> take( 1 ) -> get();    # Obtiene un objeto con un solo Array obteniendo de este sus valores
+        $professions_2 = DB :: table( 'professions' ) -> select( 'id' ) -> first();               # Obtiene el primer array del objeto
+        dd(
+            "DB :: table( 'professions' ) -> select( 'id' ) -> take( 1 ) -> get()",
+            "\$professions_1", $professions_1,                                                    # Muestra toda la estructura del Objeto Obtenido
+            "\$professions_1 -> first() ", $professions_1 -> first(),                             # Muestra el Primer Array Obtenido del Objeto
+            "\$professions_1 -> first() -> id ",$professions_1 -> first() -> id,                  # Muestra el valor obtenido del campo id del Primer Array Obtenido del Objeto
+            "DB :: table( 'professions' ) -> select( 'id' ) -> first()",
+            "\$professions_2", $professions_2,                                                    # Muestra el Primer Array Obtenido del Objeto
+            "\$professions_2 -> id", $professions_2 -> id                                         # Muestra el valor obtenido del campo id del Primer Array Obtenido del Objeto
         );
 
         /* Realiza inserciones a nuestra tabla 'users' */
