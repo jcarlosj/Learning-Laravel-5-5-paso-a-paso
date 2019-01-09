64d2b814d799696bd920058660443fd5f9214b69
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-09 11:08:33 -0500

    +Seeder: Constructor de consultas usando método value() para indicar el campo requerido
    Aclaración:
    Este método esta disponible gracias a que hemos usado el  Constructor
    de Consultas de Laravel y gracias a el podemos indicarle el nombre del
    campo del cual deseamos obtener el valor. En este caso el método select()
    esta ausente de la consulta y es por ello que lo ocupamos.
    
    Comando de ejecución de seeders:
    $ php artisan db:seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,19 +16,19 @@ class UserSeeder extends Seeder
         #$profession = DB :: select( 'SELECT id FROM professions WHERE title = ?', [ 'BackEnd Developer' ] );                # Obtiene el ID del campo donde title es "BackEnd Developer"
 
         /* Ejemplos usando el Constructor de consultas de Laravel usando el método where() */
-        $profession = DB :: table( 'professions' )                        # Obtiene un objeto con un solo Array obteniendo de este sus valores
+        $profession_id = DB :: table( 'professions' )               # Obtiene un objeto con un solo Array obteniendo de este sus valores
                          -> where(
-                             [ 'title' => 'BackEnd Developer' ]           # Argumentos: Usando un Array Asosiativo
-                         ) -> first();
+                             [ 'title' => 'BackEnd Developer' ]     # Argumentos: Usando un Array Asosiativo
+                         ) -> value( 'id' );                        # Con este método indicamos el campo del que deseamos obtener el valor
 
-        dd( $profession );
+        dd( $profession_id );
 
         /* Realiza inserciones a nuestra tabla 'users' */
         DB :: table( 'users' ) -> insert([
             'name' => 'Juan Carlos Jiménez Gutiérrez',
             'email' => 'jcjimenez29@misena.edu.co',
             'password' => bcrypt( 'laravel' ),            # bcrypt(): Helper de Laravel para encriptar contraseñas
-            'profession_id' => $profession -> id          # Insertamos un ID existente en la tabla 'professions'
+            'profession_id' => $profession_id             # Insertamos un ID existente en la tabla 'professions'
         ]);
     }
 }
