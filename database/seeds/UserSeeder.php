75d1cecc16235c7fd5eb7835df6c66f2f16e4d4d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-09 11:33:52 -0500

    +Seeder: Refactoriza consulta inserción usuario con id de profesión

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,22 +12,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        /* Ejemplo de consultas SQL usando el método select() del Facade DB de Lavavel */
-        #$profession = DB :: select( 'SELECT id FROM professions WHERE title = ?', [ 'BackEnd Developer' ] );                # Obtiene el ID del campo donde title es "BackEnd Developer"
-
-        /* Ejemplos usando el Constructor de consultas de Laravel usando el método where() */
-        $profession_id = DB :: table( 'professions' )             # Obtiene un objeto con un solo Array obteniendo de este sus valores
-                         -> whereTitle( 'BackEnd Developer' )     # Usando Métodos Mágicos con Lavavel (entenderá entonces que Title es el nombre de la columna) y pasamos solo el valor de busqueda
-                         -> value( 'id' );                        # Con este método indicamos el campo del que deseamos obtener el valor
-
-        dd( $profession_id );
-
-        /* Realiza inserciones a nuestra tabla 'users' */
+        /* Realiza inserciones a nuestra tabla 'users' con el id de la profesión */
         DB :: table( 'users' ) -> insert([
             'name' => 'Juan Carlos Jiménez Gutiérrez',
             'email' => 'jcjimenez29@misena.edu.co',
             'password' => bcrypt( 'laravel' ),            # bcrypt(): Helper de Laravel para encriptar contraseñas
-            'profession_id' => $profession_id             # Insertamos un ID existente en la tabla 'professions'
+            'profession_id' => DB :: table( 'professions' ) -> whereTitle( 'BackEnd Developer' ) -> value( 'id' )   # Insertamos un ID existente en la tabla 'professions'
         ]);
     }
 }
