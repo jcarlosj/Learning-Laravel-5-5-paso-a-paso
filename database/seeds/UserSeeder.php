edbfd34758539d3111babae8cda18fe683cd8434
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-09 10:47:06 -0500

    +Seeder: Constructor de consultas usando el método where() con 3 parámetros
    Aclaración:
    Al agregar el método where hemos pasado tres parámetros para poder
    construir el condicional que filtrará la consulta SQL que deseamos
    ejecutar.
    
    Los parámetros pasados al método tienen el siquiente propósito
      - El primero define el nombre del campo que se desea comparar
      - El segundo define el tipo de condición que deseamos aplicar
      - El tecero define el valor con el que deseamos que sea comparado

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,17 +15,20 @@ class UserSeeder extends Seeder
         /* Ejemplo de consultas SQL usando el método select() del Facade DB de Lavavel */
         #$profession = DB :: select( 'SELECT id FROM professions WHERE title = ?', [ 'BackEnd Developer' ] );                # Obtiene el ID del campo donde title es "BackEnd Developer"
 
-        /* Ejemplos usando el Constructor de consultas de Laravel */
-        $profession = DB :: table( 'professions' ) -> select( 'id' ) -> take( 1 ) -> get();    # Obtiene un objeto con un solo Array obteniendo de este sus valores
+        /* Ejemplos usando el Constructor de consultas de Laravel usando el método where() */
+        $profession = DB :: table( 'professions' ) -> select( 'id' )      # Obtiene un objeto con un solo Array obteniendo de este sus valores
+                         -> where(
+                             'title', '=', 'BackEnd Developer'            # Argumentos: primero nombre del campo, segundo operador de comparacion, tercero valor a comparar
+                         ) -> first();
 
-        #dd( $profession );
+        dd( $profession );
 
         /* Realiza inserciones a nuestra tabla 'users' */
         DB :: table( 'users' ) -> insert([
             'name' => 'Juan Carlos Jiménez Gutiérrez',
             'email' => 'jcjimenez29@misena.edu.co',
             'password' => bcrypt( 'laravel' ),            # bcrypt(): Helper de Laravel para encriptar contraseñas
-            'profession_id' => $profession -> first() -> id   # Insertamos un ID existente en la tabla 'professions'
+            'profession_id' => $profession -> id          # Insertamos un ID existente en la tabla 'professions'
         ]);
     }
 }
