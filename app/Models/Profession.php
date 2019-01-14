88925c03ffc49ef425f0b767cd8590863be719c8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-14 10:41:28 -0500

    +Model: Define campos de carga masiva para el modelo 'Profession' usando
    Aclaración:
    Al definir los campos/filas a las que podemos asignar datos de forma
    masiva estamos evitando que MassAssignmentException de Laravel que
    nos protege de la inyección masiva de registros nos permita hacerlo
    solo con los campos que definimos que deben existir dentro de la
    estructura de nuestra tabla.
    
    Para esto en el modelo agregamos el atributo $fillable para indicar
    por medio de un Array los nombres de los campos que vamos a permitir
    llenar y cargar de forma masiva.

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -8,4 +8,5 @@ class Profession extends Model
 {
     protected $table = 'tb_profesiones';
     public $timestamps = false;        # Indica a ORM Eloquent que no usaremos los campos 'created_at' y 'updated_at' en la tabla
+    protected $fillable = [ 'title' ]; # Campos o columnas que deseamos permitir cargar de forma masiva (Reiniciar Tinker si se esta usando para pruebas)
 }
