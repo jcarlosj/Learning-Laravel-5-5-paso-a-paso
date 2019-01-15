d1df38b0ad066363576103ee5032286e434ba993
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-15 14:24:53 -0500

    +Model(User): Relación pertenencia Profession/User usando hasMany()
    Aclaración:
    Para establecer esta relación se crea un método en el cual se establece
    y se retorna dicha relación de la siguiente manera
    
        public function users() {
            return $this -> hasMany( User :: class );
        }

--- app/Models/Profession.php
+++ app/Models/Profession.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Profession extends Model
 {
     protected $table = 'tb_profesiones';
     public $timestamps = false;        # Indica a ORM Eloquent que no usaremos los campos 'created_at' y 'updated_at' en la tabla
     protected $fillable = [ 'title' ]; # Campos o columnas que deseamos permitir cargar de forma masiva (Reiniciar Tinker si se esta usando para pruebas)
+
+    # Define relación entre este entidad/objeto/tabla con otro a muchos
+    public function users() {                           # Eloquent buscará un campo con el nombre de este método más el postfijo _id para hacer dicha busqueda ( profession_id )
+        return $this -> hasMany( User :: class );    # Profession "tiene muchos" Users (devolviendo un objeto de la clase User)
+    }
 }
