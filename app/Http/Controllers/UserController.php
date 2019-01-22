1e5ec921977a3bd0411683a49aedc9ab338803d8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-22 09:36:57 -0500

    $(Controller|View): Obtener/Mostrar usuarios registrados (ORM)
    Aclaración:
    El objetivo es el mismo solo se cambia la forma de acquisición de datos
    del modelo de Constructor de Consultas a de usar el ORM Eloquent

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        # Obtenemos los usuarios registrados (En este caso usando el constructor de consultas de Laravel)
-        $users = DB :: table( 'users' ) -> get();
+        # Obtenemos los usuarios registrados (En este caso usando ORM Eloquent de Laravel)
+        $users = User :: all();
         #dd( 'usuarios registrados ', $users );
 
         $title = 'Usuarios';
         #dd( compact( 'title', 'users' ) );                    # Helper de Laravel similar a ejecutar var_dump(); die(); en PHP
+
+        // return view( 'users.index' )
+        //           -> with( 'users', User::all() )
+        //           -> with( 'title', 'Usuarios' );
 
         # (Sin espacios) pues users hace referencia al directorio y el archivo así: /users/index.blade.php
         return view( 'users.index', compact( 'title', 'users' ) );  # Pasa datos a la vista que son variables locales usando sus nombres para convertirlas en un array asociativo
