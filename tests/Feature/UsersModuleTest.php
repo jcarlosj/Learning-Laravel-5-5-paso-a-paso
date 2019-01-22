94016d371e22840f160a8c05f0a551de34a40426
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-22 15:42:01 -0500

    $PHPUnit(User): Redefine pruebas para entorno con base de datos
    Aclaración:
    Una ves preparado el entorno de base de datos para pruebas estas
    deben redefinirse y eliminar todo tipo de validación estática y
    usar los valores contenidos las tablas de la base de datos de
    prueba para hacer las validaciones necesarias.
    
    Las consultas usando cualquiera de las metodolodías que Laravel
    ofrece: Consultas SQL, Constructor de Consultas, ORM Eloquent o
    Model Factories estarán obteniendo estos registros de la base
    de datos de prueba configurada en el archivo 'phpunit.xml'

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UsersModuleTest extends TestCase
 {
@@ -14,6 +16,14 @@ class UsersModuleTest extends TestCase
      */
     function it_shows_the_users_list()
     {
+        /* Registro los usuarios esperados usando Model Factories */
+        factory( User :: class ) -> create([
+            'name' => 'Elisa'
+        ]);
+        factory( User :: class ) -> create([
+            'name' => 'Juliana'
+        ]);
+
         $this -> get( '/usuarios' )          # Simula petición a la URL /usuarios
               -> assertStatus( 200 )         # Comprueba el estado de la petición
               -> assertSee( 'Usuarios' )     # Comprueba que el código fuente de la página generada se puede ser ese texto
@@ -26,7 +36,10 @@ class UsersModuleTest extends TestCase
      */
     function it_shows_a_default_message_if_the_users_list_is_empty()
     {
-        $this -> get( '/usuarios?empty' )                        # Simula petición a la URL /usuarios
+        DB :: table( 'users' ) -> truncate();
+        #User :: truncate();
+
+        $this -> get( '/usuarios' )                        # Simula petición a la URL /usuarios
               -> assertStatus( 200 )                             # Comprueba el estado de la petición
               -> assertSee( 'No hay usuarios registrados' );     # Comprueba que el código fuente de la página generada se puede ser ese texto
     }
@@ -34,7 +47,7 @@ class UsersModuleTest extends TestCase
     function it_loads_the_users_details_page()
     {
         $this -> withoutExceptionHandling();
-        
+
         $this -> get( '/usuario/5' )        # Simula petición a la URL /usuario/5
         -> assertStatus( 200 )              # Comprueba el estado de la petición
         -> assertSee( 'id: 5' );            # Comprueba que el código fuente de la página generada se puede ser ese texto
