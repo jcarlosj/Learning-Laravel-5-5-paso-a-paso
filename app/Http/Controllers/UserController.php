bc9df02147d460b9c45f431dcb434eec22edce8f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-01-27 12:17:45 -0500

    +[PHPUnit]: Reescribe el mensaje de error (en Inglés) del método validate()
    Aclaración:
    El método validate retorna mensajes por defecto para cada una de las reglas
    que se especifiquen en los campos pasados. Dichas reglas se encuentran en
    /resources/lang/en/validate.php y son las que se desplegarán cuando se produzca
    un error.
    
    Pasamos como segundo parámetro al método validate un Array asociativo con el
    nombre del campo encadenado a la regla para personalizar el mensaje y hacer
    que nuestras pruebas pasen cuando los mensaje los determinamos nosotros mismos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,6 +40,8 @@ class UserController extends Controller
     {
         $data = request() -> validate([     # Método para validar automáticamente un campo específico de un conjunto de campos extraidos de una consulta (redirigiendo automáticamente a la URL anterior)
             'name' => 'required'            # Registro al listado de errores de sesión de campo esperado y una cadena con las reglas de validación que se quieren aplicar
+        ], [
+            'name.required' => 'El nombre es obligatorio!'   # Reescribe los mensaje por defecto (en Inglés) retornados por el método validate que se encuentran en /resources/lang/en/validate.php
         ]); # Obtenemos los datos enviados a través del formulario
         #dd( $data );
 
