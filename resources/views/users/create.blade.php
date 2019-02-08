703ba482fe2b55b716807821a4eb3d770a694a96
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-08 17:54:47 -0500

    +[View]: Implementa Bootstrap 4 en formularios de crear y editar usuarios

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,41 +3,49 @@
 @section( 'title', 'Crear usuario' )        {{-- Define la sección 'title' (forma abreviada) --}}
 @section( 'content' )        {{-- Define la sección 'content' --}}
 
-    <h2>Crea usuario nuevo</h2>
+    <div class="card">
+        <h4 class="card-header">Crea usuario nuevo</h4>
+        <div class="card-body">
 
-    {{-- Valida si existen errores en la sesión para esta vista  --}}
-    @if( $errors -> any() )
-        <div class="alert alert-danger">
-            <h5>Por favor corrige los campos que se indican</h5>
+            {{-- Valida si existen errores en la sesión para esta vista  --}}
+            @if( $errors -> any() )
+                <div class="alert alert-danger">
+                    <h6>Por favor corrige los campos que se indican</h6>
+                </div>
+            @endif
+
+            <form action="{{ url( 'usuarios' ) }}" method="post">
+                {!! csrf_field() !!}
+
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input id="name" class="form-control" type="text" name="name" placeholder="Ej: Juliana Puerta" value="{{ old( 'name' ) }}">
+                    @if( $errors -> has( 'name' ) )
+                        {{-- Imprime el primer error dentro del campo nombre --}}
+                        <small class="form-text text-muted">{{ $errors -> first( 'name' ) }}</small>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="email">Correo</label>
+                    <input id="email" class="form-control" type="email" name="email" placeholder="Ej: julipuerta@correo.co" value="{{ old( 'email' ) }}">
+                    @if( $errors -> has( 'email' ) )
+                        {{-- Imprime el primer error dentro del campo email --}}
+                        <small class="form-text text-muted">{{ $errors -> first( 'email' ) }}</small>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="password">Contraseña</label>
+                    <input id="password" class="form-control" type="password" name="password" placeholder="6 o más caracteres">
+                    @if( $errors -> has( 'password' ) )
+                        {{-- Imprime el primer error dentro del campo password --}}
+                        <small class="form-text text-muted">{{ $errors -> first( 'password' ) }}</small>
+                    @endif
+                </div>
+                <button class="btn btn-primary" type="submit">Crear usuario</button>
+                <a class="btn btn-link" href="{{ route( 'users.index' ) }}">Regresar</a>
+            </form>
+
         </div>
-    @endif
-
-    <form action="{{ url( 'usuarios' ) }}" method="post">
-        {!! csrf_field() !!}
-        <label for="name">Nombre</label>
-        <input id="name" type="text" name="name" placeholder="Ej: Juliana Puerta" value="{{ old( 'name' ) }}">
-        @if( $errors -> has( 'name' ) )
-            {{-- Imprime el primer error dentro del campo nombre --}}
-            <small class="alert alert-danger">{{ $errors -> first( 'name' ) }}</small>
-        @endif
-        <br />
-        <label for="email">Correo</label>
-        <input id="email" type="email" name="email" placeholder="Ej: julipuerta@correo.co" value="{{ old( 'email' ) }}">
-        @if( $errors -> has( 'email' ) )
-            {{-- Imprime el primer error dentro del campo email --}}
-            <small class="alert alert-danger">{{ $errors -> first( 'email' ) }}</small>
-        @endif
-        <br />
-        <label for="password">Contraseña</label>
-        <input id="password" type="password" name="password" placeholder="6 o más caracteres">
-        @if( $errors -> has( 'password' ) )
-            {{-- Imprime el primer error dentro del campo password --}}
-            <small class="alert alert-danger">{{ $errors -> first( 'password' ) }}</small>
-        @endif
-        <br />
-        <button type="submit">Crear usuario</button>
-    </form>
-
-    <p><a href="{{ route( 'users.index' ) }}">Regresar</a></p>
+    </div>
 
 @endsection
