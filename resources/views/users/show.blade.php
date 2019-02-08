c4152ffb901866f30a3079e51ef7849a2968be7f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-02-08 18:01:48 -0500

    +[View]: Implementa Bootstrap 4 en mostrar datos del usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,9 +3,14 @@
 @section( 'title', $user -> name )        {{-- Define la sección 'title' (forma abreviada) --}}
 @section( 'content' )        {{-- Define la sección 'content' --}}
 
-<h2>Usuario #{{ $user -> id }}</h2>
-<p><b>Nombre:</b> {{ $user -> name }}</p>
-<p><b>Correo:</b> {{ $user -> email }}</p>
-<p><a href="{{ route( 'users.index' ) }}">Regresar</a></p>
+<div class="card">
+  <h4 class="card-header">Usuario #{{ $user -> id }}</h4>
+  <div class="card-body">
+      <h5 class="card-title">Datos del usuario</h5>
+    <p class="card-text"><b>Nombre:</b> {{ $user -> name }}</p>
+    <p class="card-text"><b>Correo:</b> {{ $user -> email }}</p>
+    <a class="btn btn-primary" href="{{ route( 'users.index' ) }}">Regresar</a>
+  </div>
+</div>
 
 @endsection
